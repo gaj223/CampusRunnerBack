ae3513c13f9fa3d2330b3e911f046dacc2ddcb24
yadiraH <yadira.hernandez021@yahoo.com>
2018-03-16 04:24:31 -0500

    update config file

--- yadi_test/TestHiveStuff/db_config.php
+++ yadi_test/TestHiveStuff/db_config.php
@@ -4,8 +4,8 @@
  * All database connection variables
  */
  
-define('DB_USER', "root"); // db user
-define('DB_PASSWORD', ""); // db password (mention your db password here)
-define('DB_DATABASE', "androidhive"); // database name
-define('DB_SERVER', "localhost"); // db server
+define('DB_USER', "yadi"); // db user
+define('DB_PASSWORD', "abc123"); // db password (mention your db password here)
+define('DB_DATABASE', "yadiTest"); // database name
+define('DB_SERVER', "13.59.142.19"); // db server
 ?>

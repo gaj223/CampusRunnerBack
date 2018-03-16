126a29a9d9ac08216140a00c2a11c6e4782dee3a
yadiraH <yadira.hernandez021@yahoo.com>
2018-03-16 04:41:59 -0500

    sql to sqli

--- yadi_test/TestHiveStuff/db_connect.php
+++ yadi_test/TestHiveStuff/db_connect.php
@@ -26,10 +26,15 @@ class DB_CONNECT {
         require_once __DIR__ . '/db_config.php';
  
         // Connecting to mysql database
-        $con = mysql_connect(DB_SERVER, DB_USER, DB_PASSWORD) or die(mysql_error());
+            $con = mysqli_connect("13.59.142.19","yadi","abc123","yadiTest");
+     
+    if (mysqli_connect_errno())
+    {
+       echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
  
         // Selecing database
-        $db = mysql_select_db(DB_DATABASE) or die(mysql_error()) or die(mysql_error());
+        // $db = mysql_select_db(DB_DATABASE) or die(mysql_error()) or die(mysql_error());
  
         // returing connection cursor
         return $con;
@@ -40,7 +45,8 @@ class DB_CONNECT {
      */
     function close() {
         // closing db connection
-        mysql_close();
+        //mysql_close();
+        mysqli_close($con);
     }
  
 }

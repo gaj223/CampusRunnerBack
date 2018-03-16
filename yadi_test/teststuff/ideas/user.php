26c4f737603da51c932c25b51ed49e6d836d5272
yadiraH <yadira.hernandez021@yahoo.com>
2018-03-16 04:11:58 -0500

    update files

---
+++ yadi_test/teststuff/ideas/user.php
@@ -0,0 +1,21 @@
+<?php
+class User{
+ 
+    // database connection and table name
+    private $conn;
+    private $table_name = "users";
+ 
+    // object properties
+    public $id;
+    public $name;
+    public $description;
+    public $price;
+    public $category_id;
+    public $category_name;
+    public $created;
+ 
+    // constructor with $db as database connection
+    public function __construct($db){
+        $this->conn = $db;
+    }
+}

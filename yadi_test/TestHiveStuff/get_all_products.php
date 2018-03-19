d161bbd329de8ba6a338c5eb509fa8de46e629f4
yadiraH <yadira.hernandez021@yahoo.com>
2018-03-19 00:31:45 -0500

    edit again

--- yadi_test/TestHiveStuff/get_all_products.php
+++ yadi_test/TestHiveStuff/get_all_products.php
@@ -29,16 +29,17 @@ if ($result->num_rows() > 0) {
 //var_dump($result);
  // for now
     while ($row = $result->fetch_assoc()) {
+        var_dump($row);
         // temp user array
-        $product = array();
-        $product["pid"] = $row["pid"];
-        $product["name"] = $row["name"];
-        $product["price"] = $row["price"];
-        $product["created_at"] = $row["created_at"];
-        $product["updated_at"] = $row["updated_at"];
+        // $product = array();
+        // $product["pid"] = $row["pid"];
+        // $product["name"] = $row["name"];
+        // $product["price"] = $row["price"];
+        // $product["created_at"] = $row["created_at"];
+        // $product["updated_at"] = $row["updated_at"];
  
-        // push single product into final response array
-        array_push($response["products"], $product);
+        // // push single product into final response array
+        // array_push($response["products"], $product);
     }
     // success
     $response["success"] = 1;

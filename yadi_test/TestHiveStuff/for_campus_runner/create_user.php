b262d0e88ae89bd185a5ea9736583d5e8e7fdae2
yadiraH <yadira.hernandez021@yahoo.com>
2018-03-19 15:16:05 -0500

    new campus runner apis

--- yadi_test/TestHiveStuff/for_campus_runner/create_user.php
+++ yadi_test/TestHiveStuff/for_campus_runner/create_user.php
@@ -16,12 +16,13 @@ $obj = json_decode($json);
 
 //place in $_POST array, kinda cheating but whatever
 foreach ($obj as $key => $value) {
-   // echo "$key => $value\n";
+    echo "$key => $value\n";
     $_POST[$key] = $value;
 }
  
 // check for required fields
 if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['abc123']) && isset($_POST['user_role']) && isset($_POST['street_address'])&& isset($_POST['phone_number'])&& isset($_POST['password'])) {
+    echo "in if";
     //made a new config file could not get object oriented one working
     require 'config.php';
     //set variables to send to query 
@@ -38,22 +39,23 @@ if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['abc123']) &
 
     // mysql inserting a new row
     $result = $con->query("INSERT INTO users(name, email, abc123, user_role, street_address, phone_number, password) VALUES('$name', '$email', '$abc123','$user_role','$street_address','$phone_number','$password')");
+    echo "after query";
  
     // check if row inserted or not
-    if ($result) {
-        // successfully inserted into database
-        $response["success"] = 1;
-        $response["message"] = "User successfully created.";
+    // if ($result) {
+    //     // successfully inserted into database
+    //     $response["success"] = 1;
+    //     $response["message"] = "User successfully created.";
  
-        // echoing JSON response
-        echo json_encode($response);
-    } else {
-        // failed to insert row
-        $response["success"] = 0;
-        $response["message"] = "Oops! An error occurred.";
+    //     // echoing JSON response
+    //     echo json_encode($response);
+    // } else {
+    //     // failed to insert row
+    //     $response["success"] = 0;
+    //     $response["message"] = "Oops! An error occurred.";
  
-        // echoing JSON response
-        echo json_encode($response);
+    //     // echoing JSON response
+    //     echo json_encode($response);
     }
 } else {
     
